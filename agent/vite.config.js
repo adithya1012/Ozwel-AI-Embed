6db2ba110d4c5b90133ca77d164ff749becd4237
SINDUJA KUNA <kunasinduja1998@gmail.com>
2025-08-22 15:19:02 -0700

    Added testing chatbot by inserting into Iframe moved to test folder

--- agent/vite.config.js
+++ agent/vite.config.js
@@ -2,10 +2,9 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   root: '.',
-  publicDir: 'src/components',
   server: {
     port: 5173,
-    open: '/test/demo/index.html'
+    open: '/index.html'
   },
   build: {
     outDir: 'dist',
